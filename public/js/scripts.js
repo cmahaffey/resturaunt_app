39123c8d6bb035680d7ccb3f476d71188ef83d33
nilocy <mahaffec@gmail.com>
2015-08-30 10:14:27 -0400

    completes styling.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,19 @@
 // $('h3').data('start_time') or something
 $(function(){
-  console.log(Date.now());
+  //
+  setInterval(function(){
+    for (var i = 0; i < $(".food-prep").length; i++) {
+      if ((((Date.now()-Date.parse($(".food-prep").eq(i).data('start-time'))))/60000)<3){
+        $(".food-prep").eq(i).css("background-color","lightgreen")
+      }else if ((((Date.now()-Date.parse($(".food-prep").eq(i).data('start-time'))))/60000)<10) {
+        $(".food-prep").eq(i).css("background-color","orange")
+      }else {
+        $(".food-prep").eq(i).css("background-color","red")
+      }
+    }
+  },1000)
 
-  if (Date.now()
-  $(".food-prep").css("background-color","lightgreen")
+
+
+
 });
